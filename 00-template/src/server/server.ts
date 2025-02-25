f5e4c68f387485a6da631770494d8ec12263f0e5
Nimira43 <n.rabone72@aol.com>
2025-02-25 23:15:49 +0000

    Created  simple Socket IO server in server.ts

---
+++ 00-template/src/server/server.ts
@@ -0,0 +1,16 @@
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const PORT = 3000
+
+const server = createServer()
+
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('User connected : ' + socket.id)
+})
+
+server.listen(PORT, () => {
+  console.log('Server is listening on port ' + PORT)
+})
