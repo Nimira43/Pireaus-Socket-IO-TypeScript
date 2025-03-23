a1e8f0dae3bd4fcf642a7f7ed73f07c552d98ef7
Nimira43 <n.rabone72@aol.com>
2025-03-23 14:24:02 +0000

    Basic module.exports

---
+++ 00-template/src/client/webpack.dev.js
@@ -0,0 +1,19 @@
+const path = reuire('path')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval',
+  devServer: {
+
+  },
+  entry: './src/client/client.ts',
+  module: {
+
+  },
+  resolve: {
+
+  },
+  output: {
+    
+  }
+}
