a78e99db63aacdb1f7cc474e7914ca686ed01c19
Nimira43 <n.rabone72@aol.com>
2025-04-06 17:27:55 +0100

    Looking at server connections and disconnections. Copy made of boilerplate.

---
+++ 01-server-connections/src/server/server.ts
@@ -0,0 +1,20 @@
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import * as express from 'express'
+import * as path from 'path'
+
+const PORT = 3000
+
+const app = express()
+app.use(express.static(path.join(__dirname, '../client')))
+
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('User is connected : ' + socket.id)
+})
+
+server.listen(PORT, () => {
+  console.log('Server is listening on Port ' + PORT)
+})
