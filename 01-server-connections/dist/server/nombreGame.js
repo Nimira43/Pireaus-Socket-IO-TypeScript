a06119af06809513f94fcce9c1747a925105a732
Nimira43 <n.rabone72@aol.com>
2025-05-31 03:14:12 +0100

    Completed nombreGame

--- 01-server-connections/dist/server/nombreGame.js
+++ 01-server-connections/dist/server/nombreGame.js
@@ -1,1 +1,17 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class nombreGame {
+    constructor() {
+        this.LuckyNumbers = {};
+    }
+    GetWinners(number) {
+        const winners = [];
+        for (let id in this.LuckyNumbers) {
+            if (number === this.LuckyNumbers[id]) {
+                winners.push(id);
+            }
+        }
+        return winners;
+    }
+}
+exports.default = nombreGame;
---
+++ 01-server-connections/src/server/nombreGame.ts
@@ -0,0 +1,15 @@
+export default class nombreGame {
+  public LuckyNumbers: { [id: string]: number} = {}
+
+  public GetWinners(number: number): string[] {
+    const winners = []
+
+    for (let id in this.LuckyNumbers) {
+      if (number === this.LuckyNumbers[id]) {
+        winners.push(id)
+      }
+    }
+    return winners
+  }
+  
+}
